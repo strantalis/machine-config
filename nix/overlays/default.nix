bc6fb3c9fb8dd6cabc82201b904bcc54a814a865
strantalis <stran58@gmail.com>
2024-12-17 20:32:29 -0500

    add ory cli overlay to fix outdated version

--- nix/overlays/default.nix
+++ nix/overlays/default.nix
@@ -4,10 +4,6 @@
 {
    # Pin Karabiner https://github.com/LnL7/nix-darwin/issues/1041
     nixpkgs.overlays = [
-      (_: prev: {
-        # https://github.com/LnL7/nix-darwin/issues/1041
-        inherit (inputs.nixpkgs-stable.legacyPackages.${prev.system}) karabiner-elements;
-      })
       (import ./ory.nix)
     ];
 }
--- nix/overlays/ory.nix
+++ nix/overlays/ory.nix
@@ -1,16 +1,25 @@
-self: super: {
-  ory = super.buildGoModule.overrideAttrs (old: rec {
-    version = "1.1.0"; # Replace with the desired version
+self: super: 
+{
+  ory = super.ory.overrideAttrs (old: rec {
+    version = "v1.1.0"; # Replace with the desired version
 
     src = self.fetchFromGitHub {
       owner = "ory";
       repo = "cli";
-      rev = "refs/tags/v${version}";
+      rev = "refs/tags/${version}";
       # Update this hash for the desired version
-      hash = "sha256-06fsw697lli9qddn901xjqz9j0x9ygj0ikkg7plnnvw2icabcazr";
+      hash = "sha256-+Su2FIuCb2vpPW/OCOTzqQOZPpY9gGRbwylSepLh2hk=";
     };
 
     # Update vendorHash for the desired version
     vendorHash = "sha256-VXaMc4VnHPljVugJyuGn8EQvNUBkEhbvepg2p7vw2EY=";
+
+    # Add ldflags for versioning
+    ldflags = [
+      "-s"
+      "-w"
+      "-X github.com/ory/cli/buildinfo.Version=${version}"
+      "-X github.com/ory/cli/buildinfo.GitHash=${src.rev}"
+    ];
   });
 }
