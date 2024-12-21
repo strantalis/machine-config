9ca58ba20167084b4f52410e1f39778980d2e458
strantalis <stran58@gmail.com>
2024-12-21 11:02:43 -0500

    fix delve override

--- nix/overlays/delve.nix
+++ nix/overlays/delve.nix
@@ -1,4 +1,13 @@
 self: super: 
 {
-  delve = import self.inputs.nixpkgs-master { inherit (final) system; }.delve;
+  delve = super.delve.overrideAttrs (old: rec {
+    version = "1.23.3"; # Replace with the desired version
+
+    src = self.fetchFromGitHub {
+    owner = "go-delve";
+    repo = "delve";
+    rev = "v${version}";
+    hash = "sha256-+qC5fFBuQchz1dMP5AezWkkD2anZshN1wIteKce0Ecw=";
+  };
+  });
 }
